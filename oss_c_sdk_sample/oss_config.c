d51bcb9329d5332959efac99e1eb3f4e1070b617
yubin.byb <yubin.byb@alibaba-inc.com>
2016-05-24 20:04:44 +0800

    merge linun&win code

--- oss_c_sdk_sample/oss_config.c
+++ oss_c_sdk_sample/oss_config.c
@@ -1,12 +1,11 @@
 #include "oss_config.h"
 
-const char OSS_ENDPOINT[] = "http://oss-cn-hangzhou.aliyuncs.com";
-const char ACCESS_KEY_ID[] = "2NeLUvmJFYbrj2Eb";
-const char ACCESS_KEY_SECRET[] = "tpKbdpzCavhbYghxHih5urCw5lkBdx";
-const char BUCKET_NAME[] = "c-sdk-test";
-const char OBJECT_NAME[] = "key";
-const char MULTIPART_UPLOAD_FILE_PATH[] = "CHANGELOG.md";
+const char OSS_ENDPOINT[] = "<your endpoint>";
+const char ACCESS_KEY_ID[] = "<your access key id>";
+const char ACCESS_KEY_SECRET[] = "<your access key secret>";
+const char BUCKET_NAME[] = "<your bucket name>";
+const char OBJECT_NAME[] = "<your object key>";
+const char MULTIPART_UPLOAD_FILE_PATH[] = "<your local file>";
 
 //surfix must be '/', only used for dir relative sample
 const char DIR_NAME[] = "dir/"; 
-
--- oss_c_sdk_test/oss_config.c
+++ oss_c_sdk_test/oss_config.c
@@ -1,6 +1,6 @@
 #include "oss_config.h"
 
-char* TEST_OSS_ENDPOINT = "http://oss-cn-hangzhou.aliyuncs.com";
-char* TEST_ACCESS_KEY_ID = "2NeLUvmJFYbrj2Eb";
-char* TEST_ACCESS_KEY_SECRET = "tpKbdpzCavhbYghxHih5urCw5lkBdx";
-char* TEST_BUCKET_NAME = "c-sdk-test-3";
+char* TEST_OSS_ENDPOINT = "<your endpoint>";
+char* TEST_ACCESS_KEY_ID = "<your access key id>";
+char* TEST_ACCESS_KEY_SECRET = "<your access key secret>";
+char* TEST_BUCKET_NAME = "<your bucket name>";
