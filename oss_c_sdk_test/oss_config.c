e695dfc1d8474e942ff96670ecaa8ac1a0ba8437
baiyubin <yubin.byb@alibaba-inc.com>
2016-06-02 11:42:37 +0800

    update test config

--- oss_c_sdk_test/oss_config.c
+++ oss_c_sdk_test/oss_config.c
@@ -1,6 +1,6 @@
 #include "oss_config.h"
 
-char* TEST_OSS_ENDPOINT = "<your endpoint>";
-char* TEST_ACCESS_KEY_ID = "<your access key id>";
-char* TEST_ACCESS_KEY_SECRET = "<your access key secret>";
-char* TEST_BUCKET_NAME = "<your bucket name>";
+char* TEST_OSS_ENDPOINT = NULL;
+char* TEST_ACCESS_KEY_ID = NULL;
+char* TEST_ACCESS_KEY_SECRET = NULL;
+char* TEST_BUCKET_NAME = NULL;
