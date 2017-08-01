f30f26d86476a9595456749206cdcc2381f42c5b
鸣镝 <yubin.byb@alibaba-inc.com>
2017-08-01 12:12:28 +0800

    Add V3.5.0

--- oss_c_sdk_sample/oss_config.c
+++ oss_c_sdk_sample/oss_config.c
@@ -1,11 +1,11 @@
 #include "oss_config.h"
 
-const char OSS_ENDPOINT[] = "http://oss-cn-hangzhou.aliyuncs.com";
-const char ACCESS_KEY_ID[] = "2NeLUvmJFYbrj2Eb";
-const char ACCESS_KEY_SECRET[] = "tpKbdpzCavhbYghxHih5urCw5lkBdx";
-const char BUCKET_NAME[] = "mingdi-hz-2";
-const char OBJECT_NAME[] = "mingdi-hz2016-12-13-10-00-00-0001";
-const char MULTIPART_UPLOAD_FILE_PATH[] = "D:\\work\\oss\\c-sdk\\everything-curl.pdf";
+const char OSS_ENDPOINT[] = "<your endpoint>";
+const char ACCESS_KEY_ID[] = "<your access key id>";
+const char ACCESS_KEY_SECRET[] = "<your access key secret>";
+const char BUCKET_NAME[] = "<your bucket name>";
+const char OBJECT_NAME[] = "<your object key>";
+const char MULTIPART_UPLOAD_FILE_PATH[] = "<your local file>";
 
 //surfix must be '/', only used for dir relative sample
-const char DIR_NAME[] = "D:\\work\\oss\\c-sdk\\tmp\\"; 
+const char DIR_NAME[] = "dir/"; 
